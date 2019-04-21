86ce8943abb1e0b4349dbe720ea5f823d12c4f50
Frederik Klovborg <klovborg@gmail.com>
2019-04-21 11:47:09 +0200

    hash passwords with bcrypt

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -3,4 +3,5 @@ import mongoose from 'mongoose'
 mongoose.connect('mongodb://127.0.0.1:27017/dnd-cs-dev', {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 })
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,14 @@
 import mongoose, { Document, Schema } from 'mongoose'
+import hashPassword from '../utils/hashPassword'
 
 export interface UserInterface extends Document {
+  [key: string]: any
   name: string
   email: string
   password: string
 }
 
-const UserSchema: Schema = new Schema({
+const userSchema: Schema = new Schema<UserInterface>({
   name: {
     type: String,
     required: true,
@@ -27,6 +29,16 @@ const UserSchema: Schema = new Schema({
   },
 })
 
-const User = mongoose.model<UserInterface>('User', UserSchema)
+userSchema.pre<UserInterface>('save', async function(next) {
+  const user = this
+
+  if (user.isModified('password')) {
+    user.password = await hashPassword(user.password)
+  }
+
+  next()
+})
+
+const User = mongoose.model<UserInterface>('User', userSchema)
 
 export default User
