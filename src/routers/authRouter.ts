4f12a79a3ae6e9a9f30dd6592947461a343420e2
Frederik Klovborg <klovborg@gmail.com>
2019-05-05 22:40:00 +0200

    init and some auth REST changes

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,19 +1,38 @@
+import bcrypt from 'bcryptjs'
 import { Router } from 'express'
+import { prisma } from '../generated/prisma-client'
 import RequestWithUser from '../interfaces/requestWithUser'
 import { AuthToken } from '../interfaces/user'
 import auth from '../middleware/auth'
 import User from '../models/user'
+import generateAuthToken from '../utils/generateAuthToken'
+import hashPassword from '../utils/hashPassword'
 
 const router = Router()
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body
+  const users = await prisma.users({ where: { email } })
+  const passwordMatch = await bcrypt.compare(password, users[0].password)
+
+  if (!passwordMatch || users.length !== 1) {
+    throw new Error('Unable to login')
+  }
 
   try {
-    const user = await User.findByCredentials(email, password)
-    const token = await user.generateAuthToken()
+    const token = generateAuthToken(users[0].id)
+    const updatedUser = await prisma.updateUser({
+      data: {
+        tokens: {
+          create: {
+            token,
+          },
+        },
+      },
+      where: { id: users[0].id },
+    })
 
-    res.send({ token, user })
+    res.send({ token, user: updatedUser })
   } catch (err) {
     res.status(400).send()
   }
@@ -46,12 +65,29 @@ router.post('/logout-all', auth, async (req: RequestWithUser, res) => {
 })
 
 router.post('/signup', async (req, res) => {
-  const user = new User(req.body)
+  // const user = new User(req.body)
+  const hashedPassword = await hashPassword(req.body.password)
+
+  const user = await prisma.createUser({
+    ...req.body,
+    password: hashedPassword,
+  })
 
   try {
-    await user.save()
-    const token = await user.generateAuthToken()
-    res.status(201).send({ token, user })
+    const token = generateAuthToken(user.id)
+
+    const newUser = await prisma.updateUser({
+      data: {
+        tokens: {
+          create: {
+            token,
+          },
+        },
+      },
+      where: { id: user.id },
+    })
+
+    res.status(201).send({ token, user: newUser })
   } catch (err) {
     res.status(400).send(err)
   }
