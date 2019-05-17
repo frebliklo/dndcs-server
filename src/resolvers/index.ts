66f7c41c96094d0a58360dd7256d0b6beb2757e0
Frederik Klovborg <klovborg@gmail.com>
2019-05-17 18:41:43 +0200

    remove resolvers that need refactoring

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,15 +1,4 @@
 import AuthResolver from './AuthResolver'
-import CharacterDataResolver from './CharacterDataResolver'
-import CharacterResolver from './CharacterResolver'
-import ClassesResolver from './ClassesResolver'
-import RaceResolver from './RaceResolver'
 import UserResolver from './UserResolver'
 
-export default [
-  AuthResolver,
-  CharacterDataResolver,
-  CharacterResolver,
-  ClassesResolver,
-  RaceResolver,
-  UserResolver,
-]
+export default [AuthResolver, UserResolver]
