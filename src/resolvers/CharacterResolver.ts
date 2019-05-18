ab23383e3fe9c7c9fa0d3e9fcaed97464fccae97
Frederik Klovborg <klovborg@gmail.com>
2019-05-19 00:10:28 +0200

    add character resolver

---
+++ src/resolvers/CharacterResolver.ts
@@ -0,0 +1,70 @@
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql'
+import { Character } from '../generated/prisma-client'
+import IApolloContext from '../interfaces/apolloContext'
+import { CreateCharacterInput } from '../types/CharacterInputs'
+import CharacterType from '../types/CharacterType'
+
+@Resolver()
+class CharacterResolver {
+  @Authorized()
+  @Query(() => CharacterType, {
+    description:
+      'Find a character belonging to the currently authenticated user',
+  })
+  async character(
+    @Arg('id') id: string,
+    @Ctx() { prisma, userId }: IApolloContext
+  ): Promise<Character> {
+    try {
+      const [character] = await prisma.characters({
+        where: { AND: [{ owner: { id: userId } }, { id }] },
+      })
+
+      return character
+    } catch (err) {
+      return null
+    }
+  }
+
+  @Authorized()
+  @Mutation(() => CharacterType, {
+    description: 'Create a character for the currently authenticated user',
+  })
+  async createCharacter(
+    @Arg('data') data: CreateCharacterInput,
+    @Ctx() { prisma, userId }: IApolloContext
+  ): Promise<Character> {
+    const character = await prisma.createCharacter({
+      ...data,
+      owner: { connect: { id: userId } },
+    })
+
+    return character
+  }
+
+  @Authorized()
+  @Mutation(() => CharacterType, {
+    description:
+      'Delete a character by id owned by the currently authenticated user',
+  })
+  async deleteCharacter(
+    @Arg('id') id: string,
+    @Ctx() { prisma, userId }: IApolloContext
+  ): Promise<Character> {
+    const [characterToDelete] = await prisma.characters({
+      where: { AND: [{ id }, { owner: { id: userId } }] },
+    })
+
+    if (!characterToDelete) {
+      throw new Error('User not found')
+    }
+
+    const deletedCharacter = await prisma.deleteCharacter({
+      id: characterToDelete.id,
+    })
+
+    return deletedCharacter
+  }
+}
+
+export default CharacterResolver
---
+++ src/types/CharacterInputs.ts
@@ -0,0 +1,100 @@
+import { Length, Max, Min } from 'class-validator'
+import { Field, InputType, Int, registerEnumType } from 'type-graphql'
+import {
+  ClassEnum,
+  DiceEnum,
+  RaceEnum,
+  SubclassEnum,
+  SubraceEnum,
+} from '../interfaces/enums'
+
+registerEnumType(DiceEnum, {
+  name: 'DiceEnum',
+  description: 'The different dice available',
+})
+
+registerEnumType(ClassEnum, {
+  name: 'ClassEnum',
+  description: 'Available classes',
+})
+
+registerEnumType(SubclassEnum, {
+  name: 'SubclassEnum',
+  description: 'Available subclasses',
+})
+
+registerEnumType(RaceEnum, {
+  name: 'RaceEnum',
+  description: 'Available races',
+})
+
+registerEnumType(SubraceEnum, {
+  name: 'SubraceEnum',
+  description: 'Available subraces',
+})
+
+@InputType()
+export class CreateCharacterInput {
+  @Field(type => Boolean, { nullable: true })
+  public: boolean
+
+  @Field(type => String)
+  @Length(1, 255)
+  name: string
+
+  @Field(type => Int, { nullable: true })
+  @Min(1)
+  @Max(20)
+  level: number
+
+  @Field(type => DiceEnum, { nullable: true })
+  hitDie: number
+
+  @Field(type => ClassEnum)
+  dndClass: ClassEnum
+
+  @Field(type => SubclassEnum, { nullable: true })
+  dndSubclass: SubclassEnum
+
+  @Field(type => RaceEnum)
+  dndRace: RaceEnum
+
+  @Field(type => SubraceEnum, { nullable: true })
+  dndSubrace: SubraceEnum
+
+  @Field(type => Int, { nullable: true })
+  abilityScoreBonus: number
+
+  @Field(type => Int, { nullable: true })
+  @Min(1)
+  @Max(20)
+  strength: number
+
+  @Field(type => Int, { nullable: true })
+  @Min(1)
+  @Max(20)
+  dexterity: number
+
+  @Field(type => Int, { nullable: true })
+  @Min(1)
+  @Max(20)
+  constitution: number
+
+  @Field(type => Int, { nullable: true })
+  @Min(1)
+  @Max(20)
+  intelligence: number
+
+  @Field(type => Int, { nullable: true })
+  @Min(1)
+  @Max(20)
+  wisdom: number
+
+  @Field(type => Int, { nullable: true })
+  @Min(1)
+  @Max(20)
+  charisma: number
+
+  @Field(type => Int, { nullable: true })
+  proficiencyBonus: number
+}
