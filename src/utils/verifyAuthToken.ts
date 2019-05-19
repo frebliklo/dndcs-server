ff664ea4951dbf6a9207ce568786fbc196eddbdc
Frederik Klovborg <klovborg@gmail.com>
2019-05-19 11:37:07 +0200

    setup env vars and some heroku prep

--- src/utils/verifyAuthToken.ts
+++ src/utils/verifyAuthToken.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 
 const verifyAuthToken = (token: string) =>
-  jwt.verify(token, 'supersecrettemporarykey')
+  jwt.verify(token, process.env.JWT_SALT)
 
 export default verifyAuthToken
